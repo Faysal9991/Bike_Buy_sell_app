3061198b2bb464fa036e85f386018aec36ddb260
User <faysaltanvir991@gmail.com>
2022-02-27 15:49:02 +0600

    row add

--- lib/homebody.dart
+++ lib/homebody.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomePageBody extends StatefulWidget {
   const HomePageBody({Key? key}) : super(key: key);
 
@@ -9,9 +10,15 @@ class HomePageBody extends StatefulWidget {
 class _HomePageBodyState extends State<HomePageBody> {
   @override
   Widget build(BuildContext context) {
+    final double height=MediaQuery.of(context).size.height;
+    final  double width=MediaQuery.of(context).size.width;
     return Column(
       children: [
-        Text("Faysal")
+        Row(
+          children: [
+            Text("Most populer",style:GoogleFonts.lato (fontSize: height*0.09),)
+          ],
+        )
       ],
     );
   }
