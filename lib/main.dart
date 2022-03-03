53e83a56b3966804bd5185a18f42323804ce1bff
User <faysaltanvir991@gmail.com>
2022-03-04 00:30:10 +0600

    google signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:bikesellapp/homebody.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 
-void main() {
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
