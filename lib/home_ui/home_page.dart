c66108d987188e98f407285b824c3efe6d4ce12b
User <faysaltanvir991@gmail.com>
2022-03-05 14:49:09 +0600

    homepage start

---
+++ lib/home_ui/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:bikesellapp/login__ui/login_controler.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+class HomePage extends StatelessWidget {
+   HomePage({Key? key}) : super(key: key);
+  final controller = Get.put(LoginController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(controller.googleAccount.value?.displayName??'',style: TextStyle(fontSize: 69),)
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'login__ui/login_page.dart';
 
 void main()async{
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: loginPage(),
     );
   }
